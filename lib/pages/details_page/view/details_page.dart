ea2fa877d9f3632aa7c0bf7d5d4154f9bf936e29
mordex-01 <jalal.zarei22@gmail.com>
2024-02-23 07:30:37 -0800

    DetailsPageViewUpdated

--- lib/pages/details_page/view/details_page.dart
+++ lib/pages/details_page/view/details_page.dart
@@ -12,44 +12,61 @@ class DetailsPage extends GetView<DetailsPageController> {
       backgroundColor: const Color.fromRGBO(220, 220, 220, 1),
       body: Column(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(8),
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(25),
-              ),
-              child: TextFormField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(25),
-                  ),
-                ),
-              ),
-            ),
+          SizedBox(
+            height: MediaQuery.sizeOf(context).height / 55,
           ),
-          Material(
-            borderRadius: BorderRadius.circular(25),
-            child: InkWell(
-              onTap: () {
-                Get.offAndToNamed(HomePage.homePageRoute);
-              },
-              borderRadius: BorderRadius.circular(25),
-              child: Container(
-                width: MediaQuery.sizeOf(context).width / 1.1,
-                height: 50,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(25),
-                ),
-                child: const Center(
-                  child: Text("Submit"),
-                ),
-              ),
-            ),
+          _buildTextField("Title", 1),
+          _buildTextField("Description", 20),
+          SizedBox(
+            height: MediaQuery.sizeOf(context).height / 4.5,
           ),
+          _buildSubmitButton(context),
         ],
       ),
     );
   }
 }
+
+Widget _buildTextField(String hintText, int? maxLines) => Padding(
+      padding: const EdgeInsets.all(8),
+      child: TextFormField(
+        maxLines: maxLines,
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: Colors.white,
+          focusColor: Colors.black,
+          hintText: hintText,
+          hintStyle: const TextStyle(fontSize: 20),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(25),
+          ),
+        ),
+      ),
+    );
+Widget _buildSubmitButton(BuildContext context) => Padding(
+      padding: const EdgeInsets.all(8),
+      child: Material(
+        borderRadius: BorderRadius.circular(25),
+        child: InkWell(
+          onTap: () {
+            Get.offAndToNamed(HomePage.homePageRoute);
+          },
+          borderRadius: BorderRadius.circular(25),
+          child: Container(
+            width: MediaQuery.sizeOf(context).width / 1,
+            height: 70,
+            decoration: BoxDecoration(
+              border: Border.all(
+                width: 0.5,
+                color: Colors.black,
+              ),
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(25),
+            ),
+            child: const Center(
+              child: Text("Submit"),
+            ),
+          ),
+        ),
+      ),
+    );
